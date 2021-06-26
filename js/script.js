f0b67d519272ddb459d0d5ebd96e4b93f1918b5a
makmizi <makaiharris@gmail.com>
2021-06-26 12:23:01 -0700

    setup todo list, setup global vars

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// TODO:
+// [ ] setup HTML and styling
+//      [ ] Log on screen that asks for a name
+//      [ ] Main game
+//      [ ] Losing screen
+//      [ ] Winning screen
+// [ ] define global variables
+// [ ] create functions that increase or decrease bar size every second
+// [ ] create functions that effect levels positively 
+// [ ] create charachter animations that occur when a button is clicked
+// [ ] create remaining work bar that, when epmty, results in winning the game
+// [ ] create function that makes you lose if any of the values reach full negativity
+
+
+function sanityCheck(){
+    console.log('I am ALIVE!');
+}
+sanityCheck();
+
+// global variables
+let workLeft = '';
+let user = {
+    name: '',
+    stress: '',
+    confidence: '',
+    bathroomometer: '',
+}
+
+// buttons
+const poop = '';
+const adderal = '';
+const flex = '';
